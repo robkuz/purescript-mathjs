379675643c389337ff1651a84b7c902dbe07cc00
Alex Mouton <thomas.mouton@gmail.com>
2018-02-20 21:55:15 -0800

    Introduced MATHJS :: EFfect

--- src/Mathjs/Util.purs
+++ src/Mathjs/Util.purs
@@ -2,6 +2,10 @@ module Mathjs.Util where
 
 import Prelude (class Eq, class Show, (==), show, (<>))
 
+import Control.Monad.Eff (kind Effect)
+
+foreign import data MATHJS :: Effect
+
 type Numbers = Array Number
 
 data InvalidSize = InvalidSize Int
